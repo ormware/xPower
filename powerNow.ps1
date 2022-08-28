d9f99c6caeea3f127b67d94b1c190ce3ba4ca18d
ormware <69240937+ormware@users.noreply.github.com>
2022-08-28 17:39:16 +0200

    Update powerNow.ps1
    Added $env:username.

--- powerNow.ps1
+++ powerNow.ps1
@@ -1,4 +1,4 @@
-﻿#Invoke-WebRequest not complete html
+#Invoke-WebRequest not complete html
 
 
 #Open Edge + url
@@ -10,7 +10,7 @@ $wshell = New-Object -ComObject wscript.shell
 
 try {
     #Delete old file
-    Remove-Item 'C:\Users\Ormar\OneDrive\Documents\Elpris\Hvad er prisen på el_.html' -ErrorAction Stop
+    Remove-Item "C:\Users\$env:username\Documents\Elpris\Hvad er prisen på el_.html" -ErrorAction Stop
 }
 catch [System.Management.Automation.ItemNotFoundException] {
     echo "no such file"
@@ -18,6 +18,7 @@ catch [System.Management.Automation.ItemNotFoundException] {
 catch {
     echo "SOME ERROR"
 }
+
 sleep 5
 $wshell.SendKeys('^{s}')
 sleep 2
@@ -25,13 +26,14 @@ $wshell.SendKeys('{ENTER}')
 sleep 10
 Stop-Process -Name "msedge"
 
-[string[]]$content = Get-Content -Path 'C:\Users\Ormar\OneDrive\Documents\Elpris\Hvad er prisen på el_.html'
+[string[]]$content = Get-Content -Path "C:\Users\$env:username\Documents\Elpris\Hvad er prisen på el_.html"
+
 
 for ($i = 0; $i -lt $content.Length; $i++) {
 
     if($content[$i] -match '<span data-v-29a6b5d4="" class="details-header__total">') {
         
-        $content[$i].Substring($content[$i].IndexOf('<span data-v-29a6b5d4="" class="details-header__total">')+54,$content[$i].LastIndexOf('</span> <div data-v-29a6b5d4') - ($content[$i].IndexOf('<span data-v-29a6b5d4="" class="details-header__total">') + 53)) >> C:\Users\Ormar\OneDrive\Documents\Elpris\pris.txt
+        $content[$i].Substring($content[$i].IndexOf('<span data-v-29a6b5d4="" class="details-header__total">')+54,$content[$i].LastIndexOf('</span> <div data-v-29a6b5d4') - ($content[$i].IndexOf('<span data-v-29a6b5d4="" class="details-header__total">') + 53)) | Out-file C:\Users\$env:username\Documents\Elpris\pris.txt -force
 
     }
 }
